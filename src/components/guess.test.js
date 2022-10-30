c6d90c2ab360e9cfd3c83112e93d3d88352ab97d
Wright Frost <wfrost@middlebury.edu>
2022-10-30 19:37:44 -0400

    trying to fix test errors

--- src/components/guess.test.js
+++ src/components/guess.test.js
@@ -2,7 +2,7 @@ test.skip("Placeholder test - replace with real tests", () => {
   test("Need to write tests");
 });
 
-describe.skip("guess: word is correct"), () => {};
+describe.skip("guess: word is correct");
 //function tests if word is correct or not
 
 describe.skip("guess: individual letters are in word");

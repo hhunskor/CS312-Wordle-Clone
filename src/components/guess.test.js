524b56f853fe14be2d3f273db23089d0df31b73a
Wright Frost <wfrost@middlebury.edu>
2022-10-30 19:38:57 -0400

    accepting failure

--- src/components/guess.test.js
+++ src/components/guess.test.js
@@ -2,8 +2,8 @@ test.skip("Placeholder test - replace with real tests", () => {
   test("Need to write tests");
 });
 
-describe.skip("guess: word is correct");
+//describe.skip("guess: word is correct");
 //function tests if word is correct or not
 
-describe.skip("guess: individual letters are in word");
+//describe.skip("guess: individual letters are in word");
 //check if individual letters are in the word

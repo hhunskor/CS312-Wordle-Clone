a2ff7bcc1c3c165d4b857814771c10f1ba490dea
Wright Frost <wfrost@middlebury.edu>
2022-11-01 12:01:17 -0400

    capitalized (or tried to capitalize) component names; added input box and submit button to index.js; set fixed correct test word

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,41 @@ import styles from "../styles/index.module.css";
 
 import testwords from "../../data/testwords.json";
 
+import { useState } from "react";
+
+import Guess from "../components/Guess";
+
 export default function Main() {
-  const correctWord = testwords[Math.floor(Math.random() * testwords.length)];
+  // const correctWord = testwords[Math.floor(Math.random() * testwords.length)];
+  const correctWord = testwords[0];
+  const [guessWord, setGuess] = useState("");
+
   console.log(`correct word to test: ${correctWord}`);
+
+  function handleGuess(guess, correctGuess) {
+    return <Guess input={guess} correctWord={correctGuess} />;
+  }
+
+  const inputBox = (
+    <input
+      type="text"
+      maxLength={5}
+      placeholder={"Guess Here"}
+      onChange={(event) => {
+        setGuess(event.target.value);
+      }}
+     />
+  );
+
+  const submit = (
+    <button
+      type="Submit"
+      disabled={guessWord.length !== 5}
+      onClick={() => handleGuess(guessWord, correctWord)}
+    >
+      Submit
+    </button>
+  );
 
   return (
     <div className={styles.container}>
@@ -17,10 +49,9 @@ export default function Main() {
 
       <main>
         <h1 className="title">Generic Project</h1>
-        <p>
-          This component is just a placeholder and should be replaced with your
-          application
-        </p>
+        <p>Wordle - Middlebury Style</p>
+        {inputBox}
+        {submit}
       </main>
 
       <footer>A 312 project</footer>

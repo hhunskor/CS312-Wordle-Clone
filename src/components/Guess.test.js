6b2fd6afeb14719cb75fe4bf4d4332f82b6b2104
Hannah Sullivan <hgsullivan@middlebury.edu>
2022-11-02 15:58:56 -0400

    Rename components to capitalized

---
+++ src/components/Guess.test.js
@@ -0,0 +1,9 @@
+test.skip("Placeholder test - replace with real tests", () => {
+  test("Need to write tests");
+});
+
+//describe.skip("guess: word is correct");
+//function tests if word is correct or not
+
+//describe.skip("guess: individual letters are in word");
+//check if individual letters are in the word

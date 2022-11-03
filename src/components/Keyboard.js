ba898f46adaf385605c9ac6d40a97cccf920527f
Hayden Hunskor <hhunskor@middlebury.edu>
2022-11-02 21:59:39 -0400

    Updates to Keyboard and style module

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -5,81 +5,99 @@ import styles from "../styles/keyboard.module.css";
 import PropTypes from "prop-types";
 
 export default function Keyboard({ correctLetters, guessedLetters }) {
-  const keyrow1 = [Q, W, E, R, T, Y, U, I, O, P].map((letter) => {
+  const keyrow1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"].map(
+    (letter) => {
+      return {
+        letter: letter,
+        color: guessedLetters.includes(letter)
+          ? correctLetters.includes(letter)
+            ? "green"
+            : "darkGray"
+          : "gray",
+      };
+    }
+  );
+
+  const keyrow2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"].map(
+    (letter) => {
+      return {
+        letter: letter,
+        color: guessedLetters.includes(letter)
+          ? correctLetters.includes(letter)
+            ? "green"
+            : "darkGray"
+          : "gray",
+      };
+    }
+  );
+
+  const keyrow3 = ["Z", "X", "C", "V", "B", "N", "M"].map((letter) => {
     return {
       letter: letter,
       color: guessedLetters.includes(letter)
         ? correctLetters.includes(letter)
           ? "green"
-          : "gray"
-        : "black",
+          : "darkGray"
+        : "gray",
     };
   });
 
-  const keyrow2 = [A, S, D, F, G, H, J, K, L].map((letter) => {
-    return {
-      letter: letter,
-      color: guessedLetters.includes(letter)
-        ? correctLetters.includes(letter)
-          ? "green"
-          : "gray"
-        : "black",
-    };
-  });
-
-  const keyrow3 = [Z, X, C, V, B, N, M].map((letter) => {
-    return {
-      letter: letter,
-      color: guessedLetters.includes(letter)
-        ? correctLetters.includes(letter)
-          ? "green"
-          : "gray"
-        : "black",
-    };
-  });
+  console.log(keyrow1);
 
   return (
-    <div>
-      <div>
+    <div className={styles.keyboard}>
+      <div className={styles.keyRow}>
         {keyrow1.map((x) => {
           return (
             <button
-              className={styles.keyButton}
-              style={`background-color:${x.color}`}
+              className={
+                x.color !== "green"
+                  ? x.color === "darkGray"
+                    ? styles.darkGrayKey
+                    : styles.grayKey
+                  : styles.greenKey
+              }
               key={`${x.letter}`}
             >
-              {" "}
-              x.letter{" "}
+              {x.letter}
             </button>
           );
         })}
       </div>
 
-      <div>
+      <div className={styles.keyRow}>
         {keyrow2.map((x) => {
           return (
             <button
-              className={styles.keyButton}
-              style={`background-color:${x.color}`}
+              className={
+                x.color !== "green"
+                  ? x.color === "darkGray"
+                    ? styles.darkGrayKey
+                    : styles.grayKey
+                  : styles.greenKey
+              }
               key={`${x.letter}`}
             >
-              {" "}
-              x.letter{" "}
+              {x.letter}
             </button>
           );
         })}
       </div>
 
-      <div>
+      <div className={styles.keyRow}>
         {keyrow3.map((x) => {
           return (
             <button
-              className={styles.keyButton}
-              style={`background-color:${x.color}`}
+              className={
+                x.color !== "green"
+                  ? x.color === "darkGray"
+                    ? styles.darkGrayKey
+                    : styles.grayKey
+                  : styles.greenKey
+              }
               key={`${x.letter}`}
             >
-              {" "}
-              x.letter{" "}
+              {x.letter}
             </button>
           );
         })}

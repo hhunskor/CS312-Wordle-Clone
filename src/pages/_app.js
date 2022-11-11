5435218c047c90effb7ca246840014ad6561fbad
Wright Frost <wfrost@middlebury.edu>
2022-11-11 15:01:00 -0500

    Added .json file with letters of alphabet to data; initialized alphabet and setAlphabet in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 /* eslint-disable  react/prop-types */
 import "../styles/globals.css";
-
+// import { useState } from "react";
+// import data from "../../data/alphabet.json";
 
 function MainApp({ Component, pageProps }) {
+  //const [alphabet,setAlphabet] = useState(data)
   return <Component {...pageProps} />;
 }
 

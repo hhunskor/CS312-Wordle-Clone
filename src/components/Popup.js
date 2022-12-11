648f05631f5f77bc82de2702078fd4619bd6063e
Hannah Sullivan <hgsullivan@middlebury.edu>
2022-12-11 11:55:24 -0500

    Added restart game button and message that pops up when a Middlebury College word is played.

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,20 +2,31 @@ import PropTypes from "prop-types";
 
 import styles from "../styles/Popup.module.css";
 
+import answers from "../../data/answersDict.json";
+
 export default function Popup({ gameOver, time, correctWord, setShowStats }) {
+  const wordData = answers.find((obj) => {
+    return obj.word.toUpperCase() === correctWord;
+  });
+  let middWord = "";
+  if (wordData.middWord === true) {
+    middWord =
+      "Congratulations! You have discovered a hidden Middlebury College word.";
+  }
   const endMessage = () => {
     if (gameOver === "win") {
       return (
         <p>
           <strong>You win!</strong> The correct word was{" "}
-          <strong>{correctWord}</strong>. Play again by refreshing the page.
+          <strong>{correctWord}</strong>. Play again!
           <p>{time}</p>
+          <p>{middWord}</p>
         </p>
       );
     } else {
       return (
         <p>
-          Out of guesses! Play again by refreshing the page.
+          Out of guesses! Keep playing to try again.
           <p>{time}</p>
         </p>
       );
@@ -33,6 +44,14 @@ export default function Popup({ gameOver, time, correctWord, setShowStats }) {
           }}
         >
           Close
+        </button>
+        <button
+          className={styles.restartBtn}
+          onClick={() => {
+            window.location.reload(true);
+          }}
+        >
+          Play Again
         </button>
       </div>
     </div>
